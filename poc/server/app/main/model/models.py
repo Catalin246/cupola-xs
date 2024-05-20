29a40b716cefcb8b6c1b1be7a1a9d969216fdaf0
Matudor7 <tdr.nsk@gmail.com>
2024-05-20 12:05:24 +0200

    rename online devices variable

--- poc/server/app/main/model/models.py
+++ poc/server/app/main/model/models.py
@@ -9,4 +9,4 @@ class WifiData(db.Model):
     __tablename__ = "wifidata"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     datetime = db.Column(db.DateTime, nullable=False)
-    onlineDevicesDaily = db.Column(db.Integer, nullable=False)
+    totalonlinedevices = db.Column(db.Integer, nullable=False)
