cf04f771e1b0ee3b10a8faec2a41d9bf78a9f00b
Matudor7 <tdr.nsk@gmail.com>
2024-05-26 15:24:03 +0200

    wifi POST endpoint + database table change

--- poc/server/app/main/model/wifi_data.py
+++ poc/server/app/main/model/wifi_data.py
@@ -5,5 +5,5 @@ class WifiData(db.Model):
 
     __tablename__ = "wifidata"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    datetime = db.Column(db.DateTime, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
     total_online_devices = db.Column(db.Integer, nullable=False)
