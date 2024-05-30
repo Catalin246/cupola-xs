5d7583bb52df5087c1bd7dcc399a94b563c5c326
Tudor <90045862+Matudor7@users.noreply.github.com>
2024-05-30 15:57:47 +0200

    serialized models

--- poc/backend/app/main/model/wifidata.py
+++ poc/backend/app/main/model/wifidata.py
@@ -1,9 +1,9 @@
-from .. import db
+# from .. import db
 
-class WifiData(db.Model):
-    """ Wifi Data Model for storing details regarding online devices over time """
+# class WifiData(db.Model):
+#     """ Wifi Data Model for storing details regarding online devices over time """
 
-    __tablename__ = "wifidata"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    datetime = db.Column(db.DateTime, nullable=False)
-    total_online_devices = db.Column(db.Integer, nullable=False)
+#     __tablename__ = "wifidata"
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     datetime = db.Column(db.DateTime, nullable=False)
+#     total_online_devices = db.Column(db.Integer, nullable=False)
