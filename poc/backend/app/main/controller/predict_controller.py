6ae252076ac8d57a6cb3a3b8bc5dbc101d9b1310
Tudor <90045862+Matudor7@users.noreply.github.com>
2024-06-04 18:05:55 +0200

    working wifi predictions

--- poc/backend/app/main/controller/predict_controller.py
+++ poc/backend/app/main/controller/predict_controller.py
@@ -12,7 +12,5 @@ prediction_wifi = WifiPredictDto.prediction_wifi
 @api.route("/wifi")
 class WifiPrediction(Resource):
     @api.doc('predict total online devices based on date')
-    @api.expect(prediction_wifi, validate=True)
     def post(self):
-        data = request.get_json(force=True)
-        return predict_wifi_data(data)
+        return predict_wifi_data()
--- poc/backend/app/main/service/prediction_service.py
+++ poc/backend/app/main/service/prediction_service.py
@@ -1,6 +1,6 @@
 from app.main import ml_model_wifi, scaler_wifi, ml_model_cinema, scaler_cinema
 
-from app.main.service.wifi_data_service import get_earliest_wifi_data
+from app.main.service.wifi_data_service import get_all_wifi_data
 
 from flask import jsonify
 
@@ -10,34 +10,23 @@ from datetime import datetime, timedelta
 import numpy as np
 import pandas as pd
 
-def predict_wifi_data(data: Dict[str, str]) -> Tuple[Dict[str, str], int]:
-    date = datetime.strptime(data['date'], '%d-%m-%Y')
+def predict_wifi_data() :
     
-    first_wifi_data = get_earliest_wifi_data()
+    wifi_data = get_all_wifi_data()
 
-    print(first_wifi_data.date)
+    wifi_devices_values = []
 
-    days_to_predict = []
+    for data in wifi_data:
+        wifi_devices_values.append(data.total_online_devices)
 
-    for i in range(0, 7):
+    X = []
 
-        days_difference = (date - first_wifi_data.date).days
+    seq_length = 7
 
-        print(days_difference)
+    for i in range(len(wifi_devices_values) - seq_length):
+        X.append(wifi_devices_values[i:i + seq_length])
 
-        days_difference_array = np.array([[days_difference]])
+    predictions = ml_model_wifi.predict(X)
 
-        print(days_difference_array)
-
-        days_to_predict.append(days_difference_array)
-
-        date += timedelta(days=1)
-
-    days_to_predict = np.concatenate(days_to_predict, axis=0)
-
-    predictions = ml_model_wifi.predict(days_to_predict)
-
-    predictions = scaler_wifi.inverse_transform(predictions)
-
-    return jsonify(predictions.tolist())
+    return jsonify(predictions.tolist()[:30])
 
